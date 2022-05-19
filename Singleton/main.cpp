7b2fe119e23ca0ad6c597e7c20e41900ad3e1810
Pritpal Chhabra <PritpalS@DEV.COM>
2022-05-19 21:06:50 +0530

    Added namespace sig for demo

--- Singleton/main.cpp
+++ Singleton/main.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
 #include "../GlobalDef.h"
-
-class Singleton
+namespace sig
 {
-private:
-
-	int connect = false;
-	Singleton()
-	{	
-		std::cout << "Object created " << std::endl;
-	}
-
-public:
-
-	static Singleton* get_instance()
+	class Singleton
 	{
-		static Singleton* obj = new Singleton();
+	private:
 
-		return obj;
-
-	}
-	
-	Connect()
-	{
-		if (connect == false )
-		{
-			std::cout << "Connected to database \n";
-			connect = true;
+		int connect = false;
+		Singleton()
+		{	
+			std::cout << "Object created " << std::endl;
 		}
 
-		return 0;
-	}
+	public:
 
-	~Singleton()
-	{
-		std::cout <<"Object destroyed \n";
-	}
+		static Singleton* get_instance()
+		{
+			static Singleton* obj = new Singleton();
+
+			return obj;
+
+		}
+		
+		Connect()
+		{
+			if (connect == false )
+			{
+				std::cout << "Connected to database \n";
+				connect = true;
+			}
+
+			return 0;
+		}
+
+		~Singleton()
+		{
+			std::cout <<"Object destroyed \n";
+		}
+	};
+
 };
 
 int main(int argc, char const *argv[])
 {
 	
-	Singleton* obj1 = Singleton::get_instance();
+	sig::Singleton* obj1 = sig::Singleton::get_instance();
 
-	Singleton* obj2 = Singleton::get_instance();
+	sig::Singleton* obj2 = sig::Singleton::get_instance();
 
 	obj2->Connect();
 	obj1->Connect();
